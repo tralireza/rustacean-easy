056ee2141930ee69025e63b146af62558dd06f70
Alireza <26859725+tralireza@users.noreply.github.com>
2020-06-03 15:00:20 +0000

    String 2399 Check Distances Between Same Letters :: None & Some()

--- string/src/lib.rs
+++ string/src/lib.rs
@@ -5,23 +5,17 @@ struct Sol2399 {}
 
 impl Sol2399 {
     pub fn check_distances(s: String, distance: Vec<i32>) -> bool {
-        let mut ends = [0; 26];
+        let mut starts = [None; 26];
 
-        let s = s.as_bytes();
-        for (i, chr) in s.iter().enumerate() {
-            ends[(*chr - b'a') as usize] = i;
-        }
-
-        s.iter()
+        s.as_bytes()
+            .iter()
             .enumerate()
-            .filter(|(i, chr)| *i < ends[(*chr - b'a') as usize])
-            .all(|(_, &chr)| {
-                if ends[(chr - b'a') as usize] <= distance[(chr - b'a') as usize] as usize {
-                    false
-                } else {
-                    s[ends[(chr - b'a') as usize] - distance[(chr - b'a') as usize] as usize - 1]
-                        == chr
+            .all(|(i, chr)| match starts[(chr - b'a') as usize] {
+                None => {
+                    starts[i] = Some(i);
+                    true
                 }
+                Some(start) => i == start + 1 + distance[(chr - b'a') as usize] as usize,
             })
     }
 }
