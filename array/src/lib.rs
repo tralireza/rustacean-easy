c4f6f194ad7c338dc8aa831dc032c6852455163b
Alireza <26859725+tralireza@users.noreply.github.com>
2020-06-04 04:49:58 +0000

    Array 2404 Most Frequest Even Element

--- array/src/lib.rs
+++ array/src/lib.rs
@@ -24,5 +24,29 @@ impl Sol2389 {
     }
 }
 
+/// 2404 Most Frequest Even Element
+struct Sol2404 {}
+
+impl Sol2404 {
+    pub fn most_frequent_even(nums: Vec<i32>) -> i32 {
+        use std::collections::BTreeMap;
+
+        let mut freqs = BTreeMap::new();
+        nums.iter().filter(|&n| n & 1 == 0).for_each(|n| {
+            freqs.entry(n).and_modify(|f| *f += 1).or_insert(1);
+        });
+
+        if let Some(xfreq) = freqs.values().max() {
+            freqs
+                .iter()
+                .find(|&(_, f)| f == xfreq)
+                .map(|(&&n, _)| n)
+                .unwrap()
+        } else {
+            -1
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests;
--- array/src/tests.rs
+++ array/src/tests.rs
@@ -11,3 +11,16 @@ fn test_2389() {
         println!(":: {rst:?}");
     }
 }
+
+#[test]
+fn test_2404() {
+    for (rst, nums) in [
+        (2, vec![0, 1, 2, 2, 4, 4, 1]),
+        (4, vec![4, 4, 4, 9, 2, 4]),
+        (-1, vec![29, 47, 21, 41, 13, 37, 25, 7]),
+    ] {
+        println!("* {nums:?}");
+        assert_eq!(Sol2404::most_frequent_even(nums), rst);
+        println!(":: {rst:?}");
+    }
+}
