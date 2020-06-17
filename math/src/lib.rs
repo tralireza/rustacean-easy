8597f279998890a8400b3abc85befad117c09a32
Alireza <26859725+tralireza@users.noreply.github.com>
2020-06-17 17:45:04 +0000

    Math 2525 Categorize Box According to Criteria
    Finished `test` profile [unoptimized + debuginfo] target(s) in 0.00s
         Running unittests src/lib.rs (target/debug/deps/math-31bde2b12bfd6a64)
    
    running 1 test
    *
    :: "Heavy"
    *
    :: "Neither"
    test tests::test_2525 ... ok
    
    test result: ok. 1 passed; 0 failed; 0 ignored; 0 measured; 1 filtered out; finished in 0.00s

--- math/src/lib.rs
+++ math/src/lib.rs
@@ -10,5 +10,26 @@ impl Sol2413 {
     }
 }
 
+/// 2525 Categorize Box According to Criteria
+struct Sol2525 {}
+
+impl Sol2525 {
+    pub fn categorize_box(length: i32, width: i32, height: i32, mass: i32) -> String {
+        match (
+            length >= 10000
+                || width >= 10000
+                || height >= 10000
+                || length as i64 * width as i64 * height as i64 >= 1e9 as i64,
+            mass >= 100,
+        ) {
+            (true, true) => "Both",
+            (true, false) => "Bulky",
+            (false, true) => "Heavy",
+            _ => "Neither",
+        }
+        .to_string()
+    }
+}
+
 #[cfg(test)]
 mod tests;
--- math/src/tests.rs
+++ math/src/tests.rs
@@ -8,3 +8,14 @@ fn test_2413() {
         println!(":: {rst:?}");
     }
 }
+
+#[test]
+fn test_2525() {
+    for (rst, length, width, height, mass) in
+        [("Heavy", 1000, 35, 700, 300), ("Neither", 200, 50, 800, 50)]
+    {
+        println!("*");
+        assert_eq!(Sol2525::categorize_box(length, width, height, mass), rst);
+        println!(":: {rst:?}");
+    }
+}
