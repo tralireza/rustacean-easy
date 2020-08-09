870e832cda9441c758cc996849510da542c6dc2f
Alireza <26859725+tralireza@users.noreply.github.com>
2020-08-09 04:24:44 +0000

    Simulation 3354 Make Array Elememnt Equal to Zero
        Finished `test` profile [unoptimized + debuginfo] target(s) in 0.12s
         Running unittests src/lib.rs (target/debug/deps/simulation-34d39d59a301f044)
    
    running 1 test
    * [1, 0, 2, 0, 3]
    :: 2
    * [2, 3, 4, 0, 4, 1, 0]
    :: 0
    * [16, 13, 10, 0, 0, 0, 10, 6, 7, 8, 7]
    :: 3
    * [0]
    :: 2
    test tests::test_3354 ... ok
    
    test result: ok. 1 passed; 0 failed; 0 ignored; 0 measured; 2 filtered out; finished in 0.00s

--- simulation/src/lib.rs
+++ simulation/src/lib.rs
@@ -1,5 +1,43 @@
 //! # Simulation
 
+/// 3354 Make Array Element Equal to Zero
+struct Sol3354 {}
+
+impl Sol3354 {
+    pub fn count_valid_selections(nums: Vec<i32>) -> i32 {
+        let prefix_sum: Vec<_> = nums
+            .iter()
+            .scan(0, |csum, n| {
+                *csum += n;
+                Some(*csum)
+            })
+            .collect();
+
+        let mut suffix_sum: Vec<_> = nums
+            .iter()
+            .rev()
+            .scan(0, |csum, n| {
+                *csum += n;
+                Some(*csum)
+            })
+            .collect();
+        suffix_sum.reverse();
+
+        (0..nums.len())
+            .filter(|&i| nums[i] == 0)
+            .map(|i| {
+                if prefix_sum[i] == suffix_sum[i] {
+                    2
+                } else if (prefix_sum[i] - suffix_sum[i]).abs() == 1 {
+                    1
+                } else {
+                    0
+                }
+            })
+            .sum()
+    }
+}
+
 /// 3379 Transformed Array
 struct Sol3379 {}
 
--- simulation/src/tests.rs
+++ simulation/src/tests.rs
@@ -1,6 +1,18 @@
 use super::*;
 
 #[test]
+fn test_3354() {
+    for (rst, nums) in [
+        (2, vec![1, 0, 2, 0, 3]),
+        (0, vec![2, 3, 4, 0, 4, 1, 0]),
+    ] {
+        println!("* {nums:?}");
+        assert_eq!(Sol3354::count_valid_selections(nums), rst);
+        println!(":: {rst:?}");
+    }
+}
+
+#[test]
 fn test_3379() {
     for (rst, nums) in [
         (vec![1, 1, 1, 3], vec![3, -2, 1, 1]),
