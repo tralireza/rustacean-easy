9472c7407443ec7e1c4ccebe925e0198d9cba67c
Alireza <26859725+tralireza@users.noreply.github.com>
2020-08-09 02:20:37 +0000

    Simulation 3379 Transformed Array :: match {}
        Finished `test` profile [unoptimized + debuginfo] target(s) in 0.00s
         Running unittests src/lib.rs (target/debug/deps/simulation-34d39d59a301f044)
    
    running 1 test
    * [3, -2, 1, 1]
    :? [1, 1, 1, 3]
    :: [1, 1, 1, 3]
    * [-1, 4, -1]
    :? [-1, -1, 4]
    :: [-1, -1, 4]
    test tests::test_3379 ... ok
    
    test result: ok. 1 passed; 0 failed; 0 ignored; 0 measured; 2 filtered out; finished in 0.00s

--- simulation/src/lib.rs
+++ simulation/src/lib.rs
@@ -6,6 +6,19 @@ struct Sol3379 {}
 impl Sol3379 {
     pub fn construct_transformed_array(nums: Vec<i32>) -> Vec<i32> {
         let m = nums.len();
+
+        println!(
+            ":? {:?}",
+            nums.iter()
+                .enumerate()
+                .map(|(i, &n)| match n {
+                    0.. => (i + n as usize) % m,
+                    ..0 => (i as i32 + n % m as i32 + m as i32) as usize % m,
+                })
+                .map(|i| nums[i])
+                .collect::<Vec<_>>()
+        );
+
         nums.iter()
             .enumerate()
             .map(|(i, &n)| (i as i32 + n % m as i32 + m as i32) as usize % m)
