3392c8f18d4fcd9032a8afd32cad7de712b17c8b
Alireza <26859725+tralireza@users.noreply.github.com>
2020-08-17 01:48:51 +0000

    Sliding Window 3411 Maximum Subarray With Equal Products :: Brute Force O(N^2) -> .scan() .inspect()
        Finished `test` profile [unoptimized + debuginfo] target(s) in 0.00s
         Running unittests src/lib.rs (target/debug/deps/slidingwindow-98143ca2d0279f3e)
    
    running 1 test
    * [1, 2, 1, 2, 1, 1, 1]
    -> (2, 1, 2, 1)
    -> (2, 1, 2, 2)
    -> (2, 1, 2, 1)
    -> (2, 1, 2, 1)
    -> (2, 1, 2, 2)
    -> (2, 1, 2, 3)
    -> (2, 1, 2, 4)
    -> (2, 1, 2, 1)
    -> (2, 1, 2, 2)
    -> (2, 1, 2, 3)
    -> (1, 1, 1, 1)
    -> (1, 1, 1, 2)
    -> (1, 1, 1, 1)
    :: 5
    * [2, 3, 4, 5, 6]
    -> (6, 1, 6, 1)
    -> (12, 1, 12, 1)
    -> (60, 1, 60, 2)
    -> (20, 1, 20, 1)
    -> (30, 1, 30, 1)
    :: 3
    * [1, 2, 3, 1, 4, 5, 1]
    -> (2, 1, 2, 1)
    -> (6, 1, 6, 2)
    -> (6, 1, 6, 3)
    -> (6, 1, 6, 1)
    -> (6, 1, 6, 2)
    -> (3, 1, 3, 1)
    -> (12, 1, 12, 2)
    -> (60, 1, 60, 3)
    -> (60, 1, 60, 4)
    -> (4, 1, 4, 1)
    -> (20, 1, 20, 2)
    -> (20, 1, 20, 3)
    -> (20, 1, 20, 1)
    -> (20, 1, 20, 2)
    -> (5, 1, 5, 1)
    :: 5
    test tests::test_3411 ... ok
    
    test result: ok. 1 passed; 0 failed; 0 ignored; 0 measured; 1 filtered out; finished in 0.00s

---
+++ slidingwindow/src/lib.rs
@@ -0,0 +1,43 @@
+//! # Sliding Window
+
+/// 3411 Maximum Subarray With Equal Products
+struct Sol3411 {}
+
+impl Sol3411 {
+    pub fn max_length(nums: Vec<i32>) -> i32 {
+        fn gcd(mut a: i32, mut b: i32) -> i32 {
+            while b != 0 {
+                (a, b) = (b, a % b);
+            }
+            a
+        }
+
+        nums.iter()
+            .take(nums.len() - 1)
+            .enumerate()
+            .fold(1, |longest, (i, &n)| {
+                nums.iter()
+                    .skip(i + 1)
+                    .enumerate()
+                    .scan((n, n, n, 0), |(prd, g, l, _), (i, &n)| {
+                        *prd *= n;
+                        *g = gcd(*g, n);
+                        *l *= n / gcd(*l, n);
+
+                        if *prd == *g * *l {
+                            Some((*prd, *g, *l, i + 1))
+                        } else {
+                            None
+                        }
+                    })
+                    .inspect(|state| println!("-> {state:?}"))
+                    .map(|(_, _, _, longest)| longest + 1)
+                    .max()
+                    .unwrap_or(0)
+                    .max(longest)
+            }) as _
+    }
+}
+
+#[cfg(test)]
+mod tests;
---
+++ slidingwindow/src/tests.rs
@@ -0,0 +1,14 @@
+use super::*;
+
+#[test]
+fn test_3411() {
+    for (rst, nums) in [
+        (5, vec![1, 2, 1, 2, 1, 1, 1]),
+        (3, vec![2, 3, 4, 5, 6]),
+        (5, vec![1, 2, 3, 1, 4, 5, 1]),
+    ] {
+        println!("* {nums:?}");
+        assert_eq!(Sol3411::max_length(nums), rst);
+        println!(":: {rst:?}");
+    }
+}
