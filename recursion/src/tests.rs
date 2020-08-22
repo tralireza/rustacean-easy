affc25ca9a66f7e911288b00527b348aaa013666
Alireza <26859725+tralireza@users.noreply.github.com>
2020-08-22 22:16:11 +0000

    Recursion 3483 Unique 3-Digit Even Numbers
        Finished `test` profile [unoptimized + debuginfo] target(s) in 0.07s
         Running unittests src/lib.rs (target/debug/deps/recursion-692748ebb6cf4d4d)
    
    running 1 test
    * [1, 2, 3, 4]
    -> {312, 214, 412, 432, 134, 314, 324, 342, 132, 124, 142, 234}
    :: 12
    * [0, 2, 2]
    -> {220, 202}
    :: 2
    * [6, 6, 6]
    -> {666}
    :: 1
    * [1, 3, 5]
    -> {}
    :: 0
    test tests::test_3483 ... ok
    
    test result: ok. 1 passed; 0 failed; 0 ignored; 0 measured; 0 filtered out; finished in 0.00s

--- recursion/src/tests.rs
+++ recursion/src/tests.rs
@@ -6,6 +6,7 @@ fn test_3483() {
         (12, vec![1, 2, 3, 4]),
         (2, vec![0, 2, 2]),
         (1, vec![6, 6, 6]),
+        (0, vec![1, 3, 5]),
     ] {
         println!("* {digits:?}");
         assert_eq!(Sol3483::total_numbers(digits), rst);
