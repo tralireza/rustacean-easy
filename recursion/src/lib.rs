346af0fb75c3543d0e2ce72f3c175a484bff0c36
Alireza <26859725+tralireza@users.noreply.github.com>
2020-08-22 22:38:52 +0000

    Recursion 3483 Unique 3-Digit Even Numbers
        Finished `test` profile [unoptimized + debuginfo] target(s) in 0.06s
         Running unittests src/lib.rs (target/debug/deps/recursion-692748ebb6cf4d4d)
    
    running 1 test
    * [1, 2, 3, 4]
    :? {134, 234, 314, 342, 214, 142, 412, 124, 324, 432, 132, 312}   12
    -> {134, 234, 314, 342, 142, 214, 412, 124, 324, 432, 132, 312}
    :: 12
    * [0, 2, 2]
    :? {220, 202}   2
    -> {220, 202}
    :: 2
    * [6, 6, 6]
    :? {666}   1
    -> {666}
    :: 1
    * [1, 3, 5]
    :? {}   0
    -> {}
    :: 0
    test tests::test_3483 ... ok
    
    test result: ok. 1 passed; 0 failed; 0 ignored; 0 measured; 0 filtered out; finished in 0.00s

--- recursion/src/lib.rs
+++ recursion/src/lib.rs
@@ -4,8 +4,26 @@
 struct Sol3483 {}
 
 impl Sol3483 {
-    pub fn total_numbers(digits: Vec<i32>) -> i32 {
+    pub fn total_numbers(mut digits: Vec<i32>) -> i32 {
         use std::collections::HashSet;
+
+        fn dsearch(r: i32, p: i32, digits: &mut [i32], pset: &mut HashSet<i32>) {
+            if r == 3 {
+                if p & 1 == 0 {
+                    pset.insert(p);
+                }
+                return;
+            }
+
+            for i in 0..digits.len() {
+                if digits[i] > 0 || digits[i] == 0 && p > 0 {
+                    let d = digits[i];
+                    digits[i] = -1;
+                    dsearch(r + 1, 10 * p + d, digits, pset);
+                    digits[i] = d;
+                }
+            }
+        }
 
         fn search(r: i32, p: i32, digits: &[i32], usage: &mut [bool], pset: &mut HashSet<i32>) {
             if r == 3 {
@@ -27,6 +45,10 @@ impl Sol3483 {
         let mut pset = HashSet::new();
         let mut usage = vec![false; digits.len()];
 
+        dsearch(0, 0, &mut digits, &mut pset);
+        println!(":? {pset:?}   {}", pset.len());
+
+        pset.clear();
         search(0, 0, &digits, &mut usage, &mut pset);
 
         println!("-> {pset:?}");
