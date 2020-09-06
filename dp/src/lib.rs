753e620aafe0099e9960764451d5d65dee76f858
Alireza <26859725+tralireza@users.noreply.github.com>
2020-09-06 03:50:52 +0000

    DP 198m House Robber :: BTreeMap
        Finished `test` profile [unoptimized + debuginfo] target(s) in 0.09s
         Running unittests src/lib.rs (target/debug/deps/dp-4bf6e15177c927a0)
    
    running 1 test
    * [1, 2, 3, 1]
    -> {0: 1, 1: 2, 2: 4} | {2: 4, 0: 1, 1: 2}
    -> {0: 1, 1: 2, 2: 4, 3: 4} | {3: 4, 1: 2, 2: 4, 0: 1}
    :: 4
    * [2, 7, 9, 3, 1]
    -> {0: 2, 1: 7, 2: 11} | {1: 7, 0: 2, 2: 11}
    -> {0: 2, 1: 7, 2: 11, 3: 11} | {2: 11, 1: 7, 0: 2, 3: 11}
    -> {0: 2, 1: 7, 2: 11, 3: 11, 4: 12} | {2: 11, 4: 12, 1: 7, 0: 2, 3: 11}
    :: 12
    test tests::test_198 ... ok
    
    test result: ok. 1 passed; 0 failed; 0 ignored; 0 measured; 0 filtered out; finished in 0.00s

--- dp/src/lib.rs
+++ dp/src/lib.rs
@@ -5,23 +5,37 @@ struct Sol198 {}
 
 impl Sol198 {
     pub fn rob(nums: Vec<i32>) -> i32 {
-        use std::collections::HashMap;
+        use std::collections::{BTreeMap, HashMap};
 
         let mut cache = HashMap::from([(0, nums[0]), (1, nums[0].max(nums[1]))]);
+        let mut sorted_cache = BTreeMap::from([(0, nums[0]), (1, nums[0].max(nums[1]))]);
 
-        fn search(i: usize, nums: &[i32], cache: &mut HashMap<usize, i32>) -> i32 {
+        fn search(
+            i: usize,
+            nums: &[i32],
+            cache: &mut HashMap<usize, i32>,
+            sorted_cache: &mut BTreeMap<usize, i32>,
+        ) -> i32 {
             if cache.contains_key(&i) {
                 return cache[&i];
             }
 
-            let value = (search(i - 2, nums, cache) + nums[i]).max(search(i - 1, nums, cache));
+            let value = (search(i - 2, nums, cache, sorted_cache) + nums[i]).max(search(
+                i - 1,
+                nums,
+                cache,
+                sorted_cache,
+            ));
+
             cache.insert(i, value);
-            println!("-> {cache:?}");
+            sorted_cache.insert(i, value);
+
+            println!("-> {sorted_cache:?} | {cache:?}");
 
             value
         }
 
-        search(nums.len() - 1, &nums, &mut cache)
+        search(nums.len() - 1, &nums, &mut cache, &mut sorted_cache)
     }
 }
 
