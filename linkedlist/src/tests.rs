5984ac7bd6aa79aabf0e1fab3c7d69f4fec2c8d2
Alireza <26859725+tralireza@users.noreply.github.com>
2020-09-28 14:50:40 +0000

    Linked List 3062 Winner of the Linked List Game
        Finished `test` profile [unoptimized + debuginfo] target(s) in 0.07s
         Running unittests src/lib.rs (target/debug/deps/linkedlist-32e21e76d2373b81)
    
    running 1 test
    * Some(ListNode3062 { val: 2, next: Some(ListNode3062 { val: 1, next: None }) })
    :: "Even"
    * Some(ListNode3062 { val: 2, next: Some(ListNode3062 { val: 5, next: Some(ListNode3062 { val: 4, next: Some(ListNode3062 { val: 7, next: Some(ListNode3062 { val: 20, next: Some(ListNode3062 { val: 5, next: None }) }) }) }) }) })
    :: "Odd"
    * Some(ListNode3062 { val: 4, next: Some(ListNode3062 { val: 5, next: Some(ListNode3062 { val: 2, next: Some(ListNode3062 { val: 1, next: None }) }) }) })
    :: "Tie"
    test tests::test_3062 ... ok
    
    test result: ok. 1 passed; 0 failed; 0 ignored; 0 measured; 0 filtered out; finished in 0.00s

--- linkedlist/src/tests.rs
+++ linkedlist/src/tests.rs
@@ -11,6 +11,25 @@ fn test_3062() {
             })),
         ),
         (
+            "Odd",
+            Some(Box::new(ListNode3062 {
+                val: 2,
+                next: Some(Box::new(ListNode3062 {
+                    val: 5,
+                    next: Some(Box::new(ListNode3062 {
+                        val: 4,
+                        next: Some(Box::new(ListNode3062 {
+                            val: 7,
+                            next: Some(Box::new(ListNode3062 {
+                                val: 20,
+                                next: Some(Box::new(ListNode3062 { val: 5, next: None })),
+                            })),
+                        })),
+                    })),
+                })),
+            })),
+        ),
+        (
             "Tie",
             Some(Box::new(ListNode3062 {
                 val: 4,
