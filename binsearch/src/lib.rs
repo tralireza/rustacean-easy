ba1bc4b20a70e068b4478ccdac09cd4298a20bf7
Alireza <26859725+tralireza@users.noreply.github.com>
2020-10-26 11:13:39 +0200

    Binary Search 1099 Two Sum Less Than K
        Finished `test` profile [unoptimized + debuginfo] target(s) in 0.13s
         Running unittests src/lib.rs (target/debug/deps/binsearch-041a509c45cbf59a)
    
    running 1 test
    * [34, 23, 1, 24, 75, 33, 54, 8] 60
    :? O(N*log(N)) 58
    :: 58
    * [10, 20, 30] 16
    :? O(N*log(N)) -1
    :: -1
    test tests::test_1099 ... ok
    
    test result: ok. 1 passed; 0 failed; 0 ignored; 0 measured; 1 filtered out; finished in 0.00s

--- binsearch/src/lib.rs
+++ binsearch/src/lib.rs
@@ -5,6 +5,24 @@ struct Sol1099 {}
 
 impl Sol1099 {
     pub fn two_sum_less_than_k(mut nums: Vec<i32>, k: i32) -> i32 {
+        {
+            nums.sort();
+
+            let mut xsum = -1;
+            let (mut l, mut r) = (0, nums.len() - 1);
+            while l < r {
+                let cur_sum = nums[l] + nums[r];
+                if cur_sum < k {
+                    xsum = xsum.max(cur_sum);
+                    l += 1;
+                } else {
+                    r -= 1;
+                }
+            }
+
+            println!(":? O(N*log(N)) {xsum}");
+        }
+
         nums.iter()
             .take(nums.len() - 1)
             .enumerate()
