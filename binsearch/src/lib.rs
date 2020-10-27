ea4aecce7b99af83e77f1c6e9cbd2b4eb0ca59c1
Alireza <26859725+tralireza@users.noreply.github.com>
2020-10-27 11:18:19 +0200

    Binary Search 1150 Cehck if a Number Is Majority in a Sorted Array
        Finished `test` profile [unoptimized + debuginfo] target(s) in 0.10s
         Running unittests src/lib.rs (target/debug/deps/binsearch-041a509c45cbf59a)
    
    running 1 test
    * [2, 4, 5, 5, 5, 5, 5, 6, 6] 5
    :? O(log(N)) true
    :? O(log(N)) true
    :: true
    * [10, 100, 101, 101] 101
    :? O(log(N)) false
    :? O(log(N)) false
    :: false
    test tests::test_1150 ... ok
    
    test result: ok. 1 passed; 0 failed; 0 ignored; 0 measured; 1 filtered out; finished in 0.00s

--- binsearch/src/lib.rs
+++ binsearch/src/lib.rs
@@ -43,6 +43,45 @@ struct Sol1150 {}
 
 impl Sol1150 {
     pub fn is_majority_element(nums: Vec<i32>, target: i32) -> bool {
+        {
+            fn lbsearch(arr: &[i32], target: i32) -> usize {
+                let (mut l, mut r) = (0, arr.len());
+                while l < r {
+                    let m = l + ((r - l) >> 1);
+                    if arr[m] < target {
+                        l = m + 1;
+                    } else {
+                        r = m;
+                    }
+                }
+
+                l
+            }
+
+            fn rbsearch(arr: &[i32], target: i32) -> usize {
+                let (mut l, mut r) = (0, arr.len());
+                while l < r {
+                    let m = l + ((r - l) >> 1);
+                    if arr[m] > target {
+                        r = m;
+                    } else {
+                        l = m + 1;
+                    }
+                }
+
+                r - 1
+            }
+
+            let l = lbsearch(&nums, target);
+            println!(
+                ":? O(log(N)) {}",
+                l < nums.len() / 2 && nums[l + nums.len() / 2] == target
+            );
+
+            let r = rbsearch(&nums, target);
+            println!(":? O(log(N)) {}", r - l >= nums.len() / 2);
+        }
+
         nums.iter().filter(|&&n| n == target).count() > nums.len() / 2
     }
 }
