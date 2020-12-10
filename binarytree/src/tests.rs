aebd1269eda13d77aeeb3ad5298a19dfe8af2da3
Alireza <26859725+tralireza@users.noreply.github.com>
2020-12-10 04:01:58 +0000

    Binary Tree 2265m Count Nodes Equal to Average of Subtree :: .map_or()
        Finished `test` profile [unoptimized + debuginfo] target(s) in 0.05s
         Running unittests src/lib.rs (target/debug/deps/binarytree-20f9a32237827165)
    
    running 1 test
    ->  0
    ->  1
    ->  3
    ->  7
    ->  8
    ->  4
    ->  9
    -> 10
    ->  2
    ->  5
    ->  6
    -> 13
    -> 14
    * Some(RefCell { value: TreeNode { val: 4, left: Some(RefCell { value: TreeNode { val: 8, left: Some(RefCell { value: TreeNode { val: 0, left: None, right: None } }), right: Some(RefCell { value: TreeNode { val: 1, left: None, right: None } }) } }), right: Some(RefCell { value: TreeNode { val: 5, left: None, right: Some(RefCell { value: TreeNode { val: 6, left: None, right: None } }) } }) } })
    :? 5
    :: 5
    * Some(RefCell { value: TreeNode { val: 1, left: None, right: None } })
    :? 1
    :: 1
    test tests::test_2265 ... ok
    
    test result: ok. 1 passed; 0 failed; 0 ignored; 0 measured; 0 filtered out; finished in 0.00s

--- binarytree/src/tests.rs
+++ binarytree/src/tests.rs
@@ -3,20 +3,24 @@ use super::*;
 #[test]
 fn test_2265() {
     fn build_tree(start: usize, vals: &[i32]) -> Option<Rc<RefCell<TreeNode>>> {
-        if vals.len() <= start || vals[start] == -1 {
-            return None;
-        }
+        println!("-> {start:>2}");
 
-        let mut n = TreeNode::new(vals[start]);
-        n.left = build_tree(2 * start + 1, vals);
-        n.right = build_tree(2 * start + 2, vals);
-
-        Some(Rc::new(RefCell::new(n)))
+        vals.get(start).map_or(None, |&val| {
+            if val == -1 {
+                None
+            } else {
+                Some(Rc::new(RefCell::new(TreeNode {
+                    val,
+                    left: build_tree(2 * start + 1, vals),
+                    right: build_tree(2 * start + 2, vals),
+                })))
+            }
+        })
     }
 
     for (rst, root) in [
         (5, build_tree(0, &[4, 8, 5, 0, 1, -1, 6][..])),
-        (1, build_tree(0, &[1][..])),
+        (1, Some(Rc::new(RefCell::new(TreeNode::new(1))))),
     ] {
         println!("* {root:?}");
         assert_eq!(Sol2265::average_of_subtree(root), rst);
